4c9264a8f3ca0e5b78ccc2d9c89aeda8ada7597f
Gabriel Marques <bielmarques922@gmail.com>
2022-04-27 09:48:31 -0300

    Ouvindo eventos uma única vez

--- 05-Events/events.js
+++ 05-Events/events.js
@@ -2,7 +2,13 @@ const { EventEmitter } = require('events')
 const evEmitter = new EventEmitter()
 
 evEmitter.on('saySomething', (message) => {
-  console.log('Eu ouvi voce: ', message)
-})
+  console.log('Eu ouvi voce no on: ', message)
+}) // on serve para ouvir sempre
+
+evEmitter.once('saySomething', (message) => {
+  console.log('Eu ouvi voce no once: ', message)
+}) // once serve para ouvir uma unica vez
 
 evEmitter.emit('saySomething', 'Gabriel')
+evEmitter.emit('saySomething', 'Giovanna')
+evEmitter.emit('saySomething', 'Edson')
